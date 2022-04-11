bbbd4665cf85ced48dc4e895c8b487d2c8b6ab15
Alfian961 <Scriptionap@hotmail.sg>
2022-04-11 16:15:57 +0800

    a
    a

--- Angular-Captsone/src/app/movie-component/movie-component.component.ts
+++ Angular-Captsone/src/app/movie-component/movie-component.component.ts
@@ -7,7 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieComponent implements OnInit {
 
+  movies: Array<any> = [];
 
+  constructor(){
+    this.movies = [
+      {
+        movieId: 1,
+        movieTitle: 'Megamind',
+        movieSummary:'Megamind is the best movie ever',
+        genre: 'Comedy',
+        actors: "Mr Guy",
+        movieDuration: 999,
+        userRating: 'assets/megamind.jpeg'
+      },
+      {
+        movieId: 2,
+        movieTitle: 'Boss Baby',
+        movieSummary:'A movie where the boss is a baby wtf',
+        genre: 'Comedy',
+        actors: "Da Babby",
+        movieDuration: 999,
+        userRating: 'assets/bossbaby.jpg'
+      },
+      {
+        movieId: 3,
+        movieTitle: 'Mnions',
+        movieSummary:'Holy shit yellow beans',
+        genre: 'Comedy',
+        actors: "Mr Guy",
+        movieDuration: 999,
+        userRating: 'assets/minions.jpg'
+      },
+    ]
   }
 
 }
