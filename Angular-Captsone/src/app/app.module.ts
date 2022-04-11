e31665d46276f71e628f4205f54a4054e59ebeea
Alfian961 <Scriptionap@hotmail.sg>
2022-04-11 15:07:57 +0800

    movie-component

--- Angular-Captsone/src/app/app.module.ts
+++ Angular-Captsone/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MovieComponentComponent } from './movie-component/movie-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponentComponent
   ],
   imports: [
     BrowserModule
