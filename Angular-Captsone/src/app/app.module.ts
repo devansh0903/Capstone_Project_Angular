64f714b68b64c8cfe9b475fd6bbe63df65cef9c7
devansh0903 <devanshn@buffalo.edu>
2022-04-11 15:46:53 +0800

    router-outlet created. Buttons added.

--- Angular-Captsone/src/app/app.module.ts
+++ Angular-Captsone/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { MoviedetailsComponent } from './moviedetails/moviedetails.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { MoviedetailsComponent } from './moviedetails/moviedetails.component';
     MoviedetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule,RouterModule.forRoot([
+      { path : '', component: MoviedetailsComponent},
+      
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
